472f5f474c5be0fedd063b24d248710702e095ee
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-24 13:39:44 -0700

    ch1 try it 02

---
+++ Chapter 01/Try It/Try 02/simple_messages.py
@@ -0,0 +1,10 @@
+# Store a message in a variable, and print that message. Then change the value
+# of your variable to a new message, and print the new message.
+
+message = "This is a message stored in a variable."
+
+print(message)
+
+message = "The value of message has changed."
+
+print(message)

13025b800c6001a3babb6df191c722fa5b235403
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-24 19:50:47 -0700

    ch1 try it 03

---
+++ Chapter 01/Try It/Try 03/personal_message.py
@@ -0,0 +1,5 @@
+# Store a person's name in a variable, and print a message to that person. Your
+# message should be simple, such as, "Hello Eric, would you like to lean some Python today?"
+
+name = "Dominic"
+print("Hello, " + name + " this is a message just for you.")

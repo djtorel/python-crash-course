7f70f7530bb452463bf460349249c5561c85084a
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-24 21:42:33 -0700

    ch1 try it 05

---
+++ Chapter 01/Try It/Try05/famous_quote.py
@@ -0,0 +1,9 @@
+# Find a quote from a famous person you admire. Print the quote and the name of
+# its author.
+
+quote = """Every gun that is made, every warship launched, every rocket fired
+        signifies, in the final sense, a theft from those who hunger and are
+        not fed, those who are cold and are not clothed."""
+author = "Dwight D. Eisenhower"
+
+print(author + " said in an infamous speech:\n\t" + quote)

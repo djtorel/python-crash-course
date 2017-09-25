c1b57ceebb44a0a3368a0fe4caddb0312080b1d8
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-24 19:53:16 -0700

    ch1 try it 04

---
+++ Chapter 01/Try It/Try 04/name_cases.py
@@ -0,0 +1,8 @@
+# Store a person's name in a variable, and then print that person's name in lowercase,
+# uppercase, and titlecase.
+
+name = "doMiNiC"
+
+print(name.lower())
+print(name.upper())
+print(name.title())

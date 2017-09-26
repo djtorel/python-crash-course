4f2394b3f652edc3527f9f89135115a4a023e758
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-25 19:29:15 -0700

    ch2 try it 09

---
+++ Chapter 02/Try It/Try 09/favorite_number.py
@@ -0,0 +1,6 @@
+# Store your favorite number in a variable. Then, using that variable, create
+# a message that reveals your favorite number.
+
+fav_number = 42
+
+print("My favorite number is " + str(fav_number) + "!")

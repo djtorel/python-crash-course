2fc7926a25946a49ef48f06ec7e305a2d8c67909
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-25 20:28:14 -0700

    ch3 try it 02

---
+++ Chapter 03/Try It/Try 02/greetings.py
@@ -0,0 +1,9 @@
+# Start with the list from previous exercise, but instead of just printing
+# each name, print a message to them. The text of each message should be
+# the same.
+
+names = ["Adam", "Mike", "Ryan", "Stephen"]
+print("Hello " + names[0] + "!")
+print("Hello " + names[1] + "!")
+print("Hello " + names[2] + "!")
+print("Hello " + names[3] + "!")

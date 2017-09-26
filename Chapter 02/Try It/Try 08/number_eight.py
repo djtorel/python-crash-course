820c552614ca68d4b482b10b054ee2f83f88ee7f
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-25 19:26:23 -0700

    ch2 try it 08

---
+++ Chapter 02/Try It/Try 08/number_eight.py
@@ -0,0 +1,8 @@
+# Write addition, subtraction, multiplication, and divition operations that
+# each result in the number 8. Be sure to enclose your operations in print to
+# see the results. You should create four lines.
+
+print(int(16 / 2))
+print(1 + 7)
+print(12 - 4)
+print(2 * 4)

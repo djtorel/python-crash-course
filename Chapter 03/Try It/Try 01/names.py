8244bcb6f11de91e456fb5e245652c592dfe7f74
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-25 20:16:37 -0700

    ch3 try it 01

---
+++ Chapter 03/Try It/Try 01/names.py
@@ -0,0 +1,9 @@
+# Store names of a few friends in a list called names. Print each name by
+# accessing each element in the list, one at a time.
+
+names = ["Adam", "Mike", "Ryan", "Stephen"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])

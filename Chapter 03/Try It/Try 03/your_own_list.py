f87badded34a4039568caf78c5e41dbd003306c7
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-25 20:37:10 -0700

    ch3 try it 03

---
+++ Chapter 03/Try It/Try 03/your_own_list.py
@@ -0,0 +1,11 @@
+# Think of your favorite mode of transportation. Make a list that stores
+# several examples. Use your list to print a series of statements about these
+# items, such as "I would like to own a Honda motorcycle"
+
+cars = ["VW Golf R", "Honda Civic Type-R", "BMW M3"]
+
+print("I would really like to drive a " + cars[2])
+
+print("I definitely want to own a " + cars[0])
+
+print("I've heard the " + cars[1] + " is pretty sweet!")

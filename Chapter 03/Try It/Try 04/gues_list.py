6a45ab5430889af65dd071e7afb14e8c4e2fca6d
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-26 19:08:31 -0700

    ch3 try it 04

---
+++ Chapter 03/Try It/Try 04/gues_list.py
@@ -0,0 +1,10 @@
+# If you could invite anyone, living or deceased, to dinner, who would you
+# invite? Make a list that includes at least three people you'd like to invite
+# to dinner. Then use your list to print a message to each person, inviting
+# them to dinner.
+
+guests = ['Albert Einstein', 'Isaac Newton', 'Alan Turing']
+
+print("Dear " + guests[0] + ",\n\tI humbly request you to join me for dinner.")
+print("Dear " + guests[1] + ",\n\tI humbly request you to join me for dinner.")
+print("Dear " + guests[2] + ",\n\tI humbly request you to join me for dinner.")

9a78b863bf113f9f4da52f7e12c469dc186a1da6
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-29 14:18:20 -0700

    ch4 try it 03 and 04

---
+++ Chapter 04/Try It/Try04/one_million.py
@@ -0,0 +1,6 @@
+# Make a list of the numbers from one to one million, and then use a for loop
+# to print the numbers.
+
+numbers = [value for value in range(1, 1000001)]
+for number in numbers:
+    print(number)

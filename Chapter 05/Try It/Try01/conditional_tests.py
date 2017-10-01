d98a778f699b6b985d1ddb6d82c8af714af955c4
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-30 19:03:19 -0700

    ch5 try it 01

---
+++ Chapter 05/Try It/Try01/conditional_tests.py
@@ -0,0 +1,48 @@
+# Write a series of conditional tests. Print a statement describing each
+# test and your prediction for the results of each test. Your code
+# should look something like this:
+
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+# • Look closely at your results, and make sure you understand why each
+# line evaluates to True or False.
+
+# • Create at least 10 tests. Have at least 5 tests evaluate to True and
+# another 5 tests evaluate to False.
+
+things = ['potato', 'computer', 'science', 'universe', 'pizza']
+
+print("Is things[0] == 'potato'? I predict True.")
+print(things[0] == 'potato')
+
+print("Is things[1] == 'computer'? I predict True.")
+print(things[1] == 'computer')
+
+print("Is things[2] == 'science'? I predict True.")
+print(things[2] == 'science')
+
+print("Is things[3] == 'universe'? I predict True.")
+print(things[3] == 'universe')
+
+print("Is things[4] == 'pizza'? I predict True.")
+print(things[4] == 'pizza')
+
+print("Is 1 > 0 and 4 >= 5? I predict False")
+print(1 > 0 and 4 >= 5)
+
+print("Is 5 == 4 or 2 == 1? I predict False")
+print(5 == 4 or 2 == 1)
+
+print("Is 1 > 3 or 1 >= 2? I predict False")
+print(1 > 3 or 1 >= 2)
+
+print("Is 'thing' == 'thing' and 'not' == 'is'? I predict False")
+print('thing' == 'thing' and 'not' == 'is')
+
+print("Is not (not True or not False) or not (not False)? I predict False")
+print(not (not True or not False) or not (not False))

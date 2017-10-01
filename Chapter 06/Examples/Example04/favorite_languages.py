6a9cc2237dd91a869a577b741e6fc7dba8bd1ef2
Dominic Torruellas <dtorruellas@gmail.com>
2017-10-01 15:12:00 -0700

    ch6 example 04

---
+++ Chapter 06/Examples/Example04/favorite_languages.py
@@ -0,0 +1,42 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+          language.title() + ".")
+
+print("\n")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+print("\n")
+
+friends = ['phil', 'sarah']
+for name in favorite_languages:
+    print(name.title())
+
+    if name in friends:
+        print(" Hi " + name.title() +
+              ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+
+print("\n")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+print("\n")
+
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+print("\n")
+
+print("The following languages have beeb mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())

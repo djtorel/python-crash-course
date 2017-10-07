54baf97bc5c4a8937cf6ab808c62184dd4c065c6
Dominic Torruellas <dtorruellas@gmail.com>
2017-10-07 16:56:44 -0700

    ch9 try it 04

--- Chapter 09/Try It/Try04/number_served.py
+++ Chapter 09/Try It/Try04/number_served.py
@@ -3,6 +3,15 @@
 # called restaurant from this class. Print the number of customers the
 # restaurant has served, and then change this value and print it again.
 
+# Add a method called set_number_served() that lets you set the number
+# of customers that have been served. Call this method with a new number
+# and print the value again.
+
+# Add a method called increment_number_served() that lets you increment
+# the number of customers who’ve been served. Call this method with any
+# number you like that could represent how many customers were served
+# in, say, a day of business.
+
 
 class Restaurant():
     """A simple way to model a restaurant"""
@@ -22,6 +31,14 @@ class Restaurant():
         """Print that the restaurant is now open"""
         print(self.name.title() + " is now open!")
 
+    def set_number_served(self, num_customers):
+        """Set the number of customers served"""
+        self.number_served = num_customers
+
+    def increment_number_served(self, num_served):
+        """Increment self.number_served by num_served amount"""
+        self.number_served += num_served
+
 
 restaurant = Restaurant("paulys pizza", "pizza")
 print(restaurant.name)
@@ -29,5 +46,12 @@ print(restaurant.type)
 print("Number served: " + str(restaurant.number_served))
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
 restaurant.number_served = 2
+print("Number served: " + str(restaurant.number_served))
+
+restaurant.set_number_served(4)
+print("Number served: " + str(restaurant.number_served))
+
+restaurant.increment_number_served(30)
 print("Number served: " + str(restaurant.number_served))

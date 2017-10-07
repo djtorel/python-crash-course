4e368368721f5eb95febec64d3e8eeeda63843d0
Dominic Torruellas <dtorruellas@gmail.com>
2017-10-07 12:48:29 -0700

    ch8 try it 09

---
+++ Chapter 08/Try It/Try09/magicians.py
@@ -0,0 +1,11 @@
+# Make a list of magician’s names. Pass the list to a function called
+# show_magicians(), which prints the name of each magician in the list.
+
+
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician.title())
+
+
+magicians = ['Houdini', 'Penn', 'Teller']
+show_magicians(magicians)
